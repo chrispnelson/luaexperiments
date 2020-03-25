9e72db6edb759520f7ea458205fefa699e1f5098
Christopher Nelson <christopher@playstudios.com>
2020-03-25 16:48:29 -0700

    Yeah

---
+++ test_exepriments/test_devinanddell_base.lua
@@ -0,0 +1,37 @@
+--#!/usr/bin/env lua
+
+--require('os')
+
+--local DnDBase = require('devinanddell_base')
+
+--local lu = require('luaunit')
+
+io.write("\nSTART..........\n")
+
+--local status, err = pcall(lu.assertTrue( true ))
+--print(err.code)
+--print(status)
+
+--lu.assertFalse( false )
+--lu.assertEvalToFalse( nil )
+--lu.assertFalse( nil )
+
+--lu.assertNotEquals("apple", 1)
+
+--lu.assertTrue( DnDBase.HasFreeSpinsRemaining() )
+--lu.assertFalse( DnDBase.HasFreeSpinsRemaining() )
+
+--lu.assertEquals(true, DnDBase.HasFreeSpinsRemaining() == true)
+
+--lu.assertEquals(false, DnDBase.HasFreeSpinsRemaining() == true)
+
+--assert(true == DnDBase.HasFreeSpinsRemaining())
+--assert(false == DnDBase.HasFreeSpinsRemaining())
+
+--local runner = lu.LuaUnit.new()
+--runner:setOutputType("junit", "junit_xml_file")
+--os.exit( runner:runSuite() )
+
+io.write("\nEND............\n")
+
+
